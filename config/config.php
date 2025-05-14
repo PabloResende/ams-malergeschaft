ecb0fa0ca6e0334fcf436ab84463e1139c0c99b4
Pablo Silva Carrilho de Resende <126430073+PabloResende@users.noreply.github.com>
2025-05-14 12:42:47 -0300

    ajustes

--- config/config.php
+++ config/config.php
@@ -12,6 +12,6 @@ try {
     die("Não foi possível conectar ao banco de dados.");
 }
 
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
